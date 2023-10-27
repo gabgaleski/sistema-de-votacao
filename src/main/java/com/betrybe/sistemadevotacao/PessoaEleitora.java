397d74ee6d9024b903a6e061565f1e25ee244c5e
Gabriel Galeski <galeski35@gmail.com>
2023-10-27 14:19:23 -0300

    feat: Implementa a classe Pessoa Eleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,19 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa{
+  private String cpf;
 
+  public PessoaEleitora(String name, String cpf) {
+    super();
+    super.setNome(name);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
