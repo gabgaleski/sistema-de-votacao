70d97ebc1ce9173042eb28218c1f61f41f1a2992
Gabriel Galeski <galeski35@gmail.com>
2023-10-27 14:13:51 -0300

    feat: implementa classe Pessoa Candidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,29 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
+
+  public PessoaCandidata(String nome, int numero) {
+    super();
+    this.setNome(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
 }
